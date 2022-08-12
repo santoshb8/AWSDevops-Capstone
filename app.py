39111f3652f40fce128b14c37d2688845466e156
Santosh kumar Bammidi <44927005+santoshb8@users.noreply.github.com>
2022-08-12 17:31:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,19 +1,13 @@
 """
-Main application fie
+Main application file
 """
 from flask import Flask
-import logging
 app = Flask(__name__)
-
-# Initialize Loggerr
-LOGGER = logging.getLogger()
-LOGGER.setLevel(logging.INFO)
 
 @app.route('/<random_string>')
 def returnBackwardsString(random_string):
     """Reverse and return the provided URI"""
-    LOGGER.info('Received a message: %s', random_string)
-    return "".join(reversed(random_string))
+    return "Breaking the unit test"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
